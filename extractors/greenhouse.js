681f9b4de94c4a75292c5b222d805397b0f2bd26
yvamshi667 <yvamshi667@gmail.com>
2025-12-30 00:54:35 -0600

    Update greenhouse.js

--- extractors/greenhouse.js
+++ extractors/greenhouse.js
@@ -1,8 +1,18 @@
-import { scrapeGreenhouse } from "./greenhouse.js";
+import fetch from "node-fetch";
 
-export async function scrapeCompany(company) {
-  if (company.careers_url.includes("greenhouse.io")) {
-    return scrapeGreenhouse(company);
-  }
-  return [];
+export async function scrapeGreenhouse(company) {
+  const api = `${company.careers_url}/jobs?content=true`;
+
+  const res = await fetch(api);
+  if (!res.ok) return [];
+
+  const data = await res.json();
+
+  return data.jobs.map(j => ({
+    title: j.title,
+    company: company.name,
+    country: company.country || "US",
+    url: j.absolute_url,
+    source: "greenhouse"
+  }));
 }

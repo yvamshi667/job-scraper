fb477e0455605080fecab2a0fb53cae4ccef84c5
yvamshi667 <yvamshi667@gmail.com>
2025-12-30 16:47:51 -0600

    Update router.js

--- extractors/router.js
+++ extractors/router.js
@@ -1,19 +1,24 @@
-import { scrapeGreenhouse } from "./greenhouse.js";
-import { scrapeLever } from "./lever.js";
-import { scrapeAshby } from "./ashby.js";
 import { detectPageType } from "../detect.js";
+import { scrapeGreenhouse } from "./greenhouse.js";
+// future:
+// import { scrapeLever } from "./lever.js";
+// import { scrapeAshby } from "./ashby.js";
 
 export async function scrapeCompany(company) {
   const type = detectPageType(company.careers_url);
 
   switch (type) {
     case "GREENHOUSE":
-      return scrapeGreenhouse(company);
-    case "LEVER":
-      return scrapeLever(company);
-    case "ASHBY":
-      return scrapeAshby(company);
+      return await scrapeGreenhouse(company);
+
+    // case "LEVER":
+    //   return await scrapeLever(company);
+
+    // case "ASHBY":
+    //   return await scrapeAshby(company);
+
     default:
+      console.log(`Skipping unsupported type: ${company.careers_url}`);
       return [];
   }
 }

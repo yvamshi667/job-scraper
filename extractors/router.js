0d144c48f5e8386361fd93f0a0e4889562e28ad7
yvamshi667 <yvamshi667@gmail.com>
2025-12-30 16:39:50 -0600

    Update router.js

--- extractors/router.js
+++ extractors/router.js
@@ -1,8 +1,19 @@
 import { scrapeGreenhouse } from "./greenhouse.js";
+import { scrapeLever } from "./lever.js";
+import { scrapeAshby } from "./ashby.js";
+import { detectPageType } from "../detect.js";
 
 export async function scrapeCompany(company) {
-  if (company.careers_url.includes("greenhouse.io")) {
-    return scrapeGreenhouse(company);
+  const type = detectPageType(company.careers_url);
+
+  switch (type) {
+    case "GREENHOUSE":
+      return scrapeGreenhouse(company);
+    case "LEVER":
+      return scrapeLever(company);
+    case "ASHBY":
+      return scrapeAshby(company);
+    default:
+      return [];
   }
-  return [];
 }

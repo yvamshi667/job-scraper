61ed959f98b6c993dbecc832b61520280e3c2e5b
yvamshi667 <yvamshi667@gmail.com>
2025-12-30 20:26:22 -0600

    Delete detect.js

--- detect.js
+++
@@ -1,9 +0,0 @@
-export function detectPageType(url) {
-  if (!url) return "UNKNOWN";
-
-  if (url.includes("greenhouse.io")) return "GREENHOUSE";
-  if (url.includes("lever.co")) return "LEVER";
-  if (url.includes("ashbyhq.com")) return "ASHBY";
-
-  return "UNKNOWN";
-}

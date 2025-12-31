50c3dd25a7fa6a8c2667407b6e400b1ef6e3e980
yvamshi667 <yvamshi667@gmail.com>
2025-12-30 19:56:51 -0600

    Update detect.js

--- detect.js
+++ detect.js
@@ -5,5 +5,5 @@ export function detectPageType(url) {
   if (url.includes("lever.co")) return "LEVER";
   if (url.includes("ashbyhq.com")) return "ASHBY";
 
-  return "CUSTOM_PUBLIC";
+  return "UNKNOWN";
 }

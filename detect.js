0adb3b2af905b0c1675939640140e607d0a2b732
yvamshi667 <yvamshi667@gmail.com>
2025-12-30 21:26:01 -0600

    Update detect.js

--- detect.js
+++ detect.js
@@ -1,9 +1,6 @@
-export default function detectATS(url) {
-  if (!url) return null;
-
+export default function detectATS(url = "") {
   if (url.includes("greenhouse.io")) return "greenhouse";
   if (url.includes("lever.co")) return "lever";
   if (url.includes("ashbyhq.com")) return "ashby";
-
   return null;
 }

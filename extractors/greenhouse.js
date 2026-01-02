fd7e40663144134003219bf2339ca4b3781a516c
yvamshi667 <yvamshi667@gmail.com>
2026-01-02 11:34:44 -0600

    Update greenhouse.js

--- extractors/greenhouse.js
+++ extractors/greenhouse.js
@@ -1,15 +1,14 @@
-// extractors/greenhouse.js
 import fetch from "node-fetch";
 
 export async function scrapeGreenhouse(company) {
   try {
-    const res = await fetch(company.url);
+    const res = await fetch(company.careers_url);
     const data = await res.json();
 
     return (data.jobs || []).map(job => ({
       title: job.title,
-      location: job.location?.name || "Unknown",
       company: company.name,
+      location: job.location?.name || "Unknown",
       url: job.absolute_url,
       ats_source: "greenhouse",
       is_direct: true,

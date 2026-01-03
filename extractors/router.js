061c2a38621769ecdacf433bb3a98beea0f641d6
yvamshi667 <yvamshi667@gmail.com>
2026-01-03 04:18:47 -0600

    Update router.js

--- extractors/router.js
+++ extractors/router.js
@@ -1,14 +1,17 @@
+import scrapeGeneric from "./scrapeGeneric.js";
 import scrapeGreenhouse from "./greenhouse.js";
 import scrapeAshby from "./ashby.js";
 import scrapeLever from "./lever.js";
-import scrapeGeneric from "./scrapeGeneric.js";
 
-export default async function scrapeCompany(company) {
-  const url = company.careers_url.toLowerCase();
-
-  if (url.includes("greenhouse")) return scrapeGreenhouse(company);
-  if (url.includes("ashbyhq")) return scrapeAshby(company);
-  if (url.includes("lever")) return scrapeLever(company);
-
-  return scrapeGeneric(company);
+export async function routeScraper(company) {
+  switch (company.ats) {
+    case "greenhouse":
+      return scrapeGreenhouse(company);
+    case "ashby":
+      return scrapeAshby(company);
+    case "lever":
+      return scrapeLever(company);
+    default:
+      return scrapeGeneric(company);
+  }
 }

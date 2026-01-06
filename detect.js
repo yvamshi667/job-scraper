a185b193cb5d13898d84326204f37db01a2658e9
yvamshi667 <yvamshi667@gmail.com>
2026-01-05 23:31:24 -0600

    Update detect.js

--- detect.js
+++ detect.js
@@ -1,27 +1,17 @@
-import { JSDOM } from "jsdom";
-
 export async function detectCareersPage(domain) {
-  try {
-    const res = await fetch(domain, { redirect: "follow" });
-    if (!res.ok) return null;
+  const candidates = [
+    "/careers",
+    "/jobs",
+    "/join",
+    "/careers/jobs"
+  ];
 
-    const html = await res.text();
-    const dom = new JSDOM(html);
-    const links = [...dom.window.document.querySelectorAll("a")];
-
-    const careersLink = links.find(a =>
-      a.href &&
-      /careers|jobs|join/i.test(a.href)
-    );
-
-    if (!careersLink) return null;
-
-    const url = careersLink.href.startsWith("http")
-      ? careersLink.href
-      : new URL(careersLink.href, domain).href;
-
-    return url;
-  } catch {
-    return null;
+  for (const path of candidates) {
+    try {
+      const res = await fetch(domain + path, { method: "HEAD" });
+      if (res.ok) return domain + path;
+    } catch (_) {}
   }
+
+  return null;
 }

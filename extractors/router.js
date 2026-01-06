c1ea9c1c9f5bd696f1ef6c968846480028304866
yvamshi667 <yvamshi667@gmail.com>
2026-01-05 23:51:52 -0600

    Update router.js

--- extractors/router.js
+++ extractors/router.js
@@ -1,12 +1,23 @@
 import scrapeGeneric from "./scrapeGeneric.js";
 import scrapeAshby from "./ashby.js";
 
-export default async function router(company) {
+/**
+ * Routes a company to the correct scraper
+ * Safe defaults
+ * No crashes
+ * Node 20 compatible
+ */
+export default async function routeScraper(company) {
+  if (!company || !company.careers_url) {
+    return [];
+  }
+
   switch (company.ats) {
     case "ashby":
-      return scrapeAshby(company);
+      return await scrapeAshby(company);
+
     case "generic":
     default:
-      return scrapeGeneric(company);
+      return await scrapeGeneric(company);
   }
 }

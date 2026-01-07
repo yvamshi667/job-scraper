3cf8501a202a47809f36d323b79063dc73057582
yvamshi667 <yvamshi667@gmail.com>
2026-01-06 23:48:58 -0600

    Update router.js

--- extractors/router.js
+++ extractors/router.js
@@ -1,21 +1,15 @@
-// extractors/router.js
 import { greenhouse } from "./greenhouse.js";
-import { ashby } from "./ashby.js";
-import { scrapeGithub } from "./scrapeGithub.js";
+import { lever } from "./lever.js";
 
 export async function routeCompany(company) {
   switch (company.ats) {
     case "greenhouse":
       return await greenhouse(company);
 
-    case "ashby":
-      return await ashby(company);
-
-    case "github":
-      return await scrapeGithub();
+    case "lever":
+      return await lever(company);
 
     default:
-      console.warn("⚠️ Unknown ATS:", company.ats);
       return [];
   }
 }

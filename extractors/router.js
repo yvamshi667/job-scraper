4050b831915be90717e6aa8f363297a4dc413135
yvamshi667 <yvamshi667@gmail.com>
2026-01-06 20:08:01 -0600

    Update router.js

--- extractors/router.js
+++ extractors/router.js
@@ -1,7 +1,7 @@
 // extractors/router.js
 import { greenhouse } from "./greenhouse.js";
-import { scrapeGithub } from "./scrapeGithub.js";
 import { ashby } from "./ashby.js";
+import { scrapeGithub } from "./scrapeGithub.js";
 
 export async function routeCompany(company) {
   switch (company.ats) {
